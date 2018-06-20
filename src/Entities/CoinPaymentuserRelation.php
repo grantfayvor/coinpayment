0da5cab35cee3d8a204aa668612197d64ef8796a
Harrison <30683570+grantfayvor@users.noreply.github.com>
2018-06-20 11:26:34 +0100

    adde files via upload

---
+++ src/Entities/CoinPaymentuserRelation.php
@@ -0,0 +1,11 @@
+<?php
+
+  namespace Harrison\CoinPayment\Entities;
+
+  trait CoinPaymentuserRelation {
+
+    public function coinpayment_transactions(){
+      return $this->hasMany(cointpayment_log_trx::class, 'user_id');
+    }
+
+  }
